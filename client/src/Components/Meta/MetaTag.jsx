0adae5ff4373563a1f98ea8a0e92a7f1db97984e
Coderhitesh <hiteshy468@gmail.com>
2026-09-18 18:10:06 +0530

    add

--- client/src/Components/Meta/MetaTag.jsx
+++ client/src/Components/Meta/MetaTag.jsx
@@ -1,33 +1,50 @@
-// src/components/MetaTag.js
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import PropTypes from "prop-types";
 
-const MetaTag = ({ title, description, keyword, focusKeywords, robots }) => {
+const MetaTag = ({
+  title,
+  description,
+  keyword,
+  focusKeywords,
+  robots,
+  canonical,
+}) => {
   return (
     <Helmet>
       <meta charSet="utf-8" />
+
       <title>{title}</title>
+
       <meta name="description" content={description} />
+
       <meta name="keywords" content={keyword} />
+
       <meta name="focus-keywords" content={focusKeywords} />
+
       <meta name="robots" content={robots} />
+
+      {/* Canonical URL */}
+      {canonical && <link rel="canonical" href={canonical} />}
     </Helmet>
   );
 };
 
-// Add propTypes for validation
+// PropTypes
 MetaTag.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   keyword: PropTypes.string.isRequired,
   focusKeywords: PropTypes.string,
   robots: PropTypes.string,
+  canonical: PropTypes.string,
 };
 
-// Optional defaultProps to avoid undefined values
+// Default values
 MetaTag.defaultProps = {
-  focusKeywords: '',
+  focusKeywords: "",
+  robots: "index, follow",
+  canonical: "",
 };
 
 export default MetaTag;
