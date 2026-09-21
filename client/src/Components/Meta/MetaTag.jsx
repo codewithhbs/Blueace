7e8dd81576a9b09e8fc2752fed6c649a5faea7ea
Coderhitesh <hiteshy468@gmail.com>
2026-09-21 12:41:32 +0530

    add

--- client/src/Components/Meta/MetaTag.jsx
+++ client/src/Components/Meta/MetaTag.jsx
@@ -9,23 +9,33 @@ const MetaTag = ({
   focusKeywords,
   robots,
   canonical,
+  ogTitle,
+  ogDescription,
+  ogImage,
+  ogType,
 }) => {
   return (
     <Helmet>
       <meta charSet="utf-8" />
-
       <title>{title}</title>
-
       <meta name="description" content={description} />
-
       <meta name="keywords" content={keyword} />
-
       <meta name="focus-keywords" content={focusKeywords} />
-
       <meta name="robots" content={robots} />
-
-      {/* Canonical URL */}
       {canonical && <link rel="canonical" href={canonical} />}
+
+      {/* Open Graph */}
+      <meta property="og:title" content={ogTitle || title} />
+      <meta property="og:description" content={ogDescription || description} />
+      <meta property="og:type" content={ogType} />
+      {canonical && <meta property="og:url" content={canonical} />}
+      {ogImage && <meta property="og:image" content={ogImage} />}
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={ogTitle || title} />
+      <meta name="twitter:description" content={ogDescription || description} />
+      {ogImage && <meta name="twitter:image" content={ogImage} />}
     </Helmet>
   );
 };
@@ -38,6 +48,10 @@ MetaTag.propTypes = {
   focusKeywords: PropTypes.string,
   robots: PropTypes.string,
   canonical: PropTypes.string,
+  ogTitle: PropTypes.string,
+  ogDescription: PropTypes.string,
+  ogImage: PropTypes.string,
+  ogType: PropTypes.string,
 };
 
 // Default values
@@ -45,6 +59,10 @@ MetaTag.defaultProps = {
   focusKeywords: "",
   robots: "index, follow",
   canonical: "",
+  ogTitle: "",
+  ogDescription: "",
+  ogImage: "",
+  ogType: "website",
 };
 
 export default MetaTag;
